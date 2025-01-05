af36d98bdd88cca79807cbfd7cfe4ae8a5924bd8
Julia Johnson <jjdev2024@outlook.com>
2025-01-05 12:32:38 -0500

    Initial Commit

---
+++ API/Controllers/Products.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(StoreContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await context.Products.FindAsync(id) 
+            ?? throw new Exception("Product not found");
+    }
+}
